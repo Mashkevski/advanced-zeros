ae63c984617a355a6c30a1520ce54ffd99b66d01
Siarhei Mashkevski <cybera@list.ru>
2019-03-13 22:45:02 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,58 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
-}
+
+  const primeFactors = [];
+  const zerosCount = [];
+
+  const getOddMultiplier = function (N) {
+    for (let i = 3; i * i <= N; i += 2) {
+      let n = 0;
+      while (N % i === 0) {
+        N /= i;
+        n++;
+      }
+      if (n > 0) {
+        primeFactors.push([i, n]);
+      }
+    }
+    if (N > 1) {
+      primeFactors.push([N, 1]);
+    }
+  };
+
+  const getEvenMultiplier = function () {
+    let N = base;
+    let n = 0;
+
+    while (N % 2 === 0) {
+      N /= 2;
+      n++;
+    }
+
+    if (n > 0) {
+      primeFactors.push([2, n]);
+    }
+
+    getOddMultiplier(N);
+  };
+
+  const count = function (delimeter) {
+    let zeroCount = 0;
+    let divider = delimeter[0];
+
+    for (let i = 1; divider <= number; i++) {
+      divider = Math.pow(delimeter[0], i);
+      zeroCount += Math.floor(number / divider);
+    }
+
+    return zeroCount;
+  };
+
+  getEvenMultiplier();
+
+  for (let i = 0; i < primeFactors.length; i++) {
+    zerosCount.push(Math.floor(count(primeFactors[i]) / primeFactors[i][1]));
+  }
+
+  return Math.min(...zerosCount);
+};
+
