7b4f36d7cad3e7c87fce55a65d14d61bc91a8c00
Siarhei Mashkevski <cybera@list.ru>
2019-03-13 23:14:58 +0300

    refactor: change names

--- src/index.js
+++ src/index.js
@@ -1,21 +1,24 @@
 module.exports = function getZerosCount(number, base) {
 
-  const primeFactors = [];
+  const basePrimeFactors = [];
   const zerosCount = [];
 
   const getOddMultiplier = function (N) {
     for (let i = 3; i * i <= N; i += 2) {
       let n = 0;
+
       while (N % i === 0) {
         N /= i;
         n++;
       }
+
       if (n > 0) {
-        primeFactors.push([i, n]);
+        basePrimeFactors.push({primeFactor: i, degree: n});
       }
     }
+
     if (N > 1) {
-      primeFactors.push([N, 1]);
+      basePrimeFactors.push({primeFactor: N, degree: 1});
     }
   };
 
@@ -29,18 +32,18 @@ module.exports = function getZerosCount(number, base) {
     }
 
     if (n > 0) {
-      primeFactors.push([2, n]);
+      basePrimeFactors.push({primeFactor: 2, degree: n});
     }
 
     getOddMultiplier(N);
   };
 
-  const count = function (delimeter) {
+  const count = function (div) {
     let zeroCount = 0;
-    let divider = delimeter[0];
+    let divider = div.primeFactor;
 
     for (let i = 1; divider <= number; i++) {
-      divider = Math.pow(delimeter[0], i);
+      divider = Math.pow(div.primeFactor, i);
       zeroCount += Math.floor(number / divider);
     }
 
@@ -49,10 +52,9 @@ module.exports = function getZerosCount(number, base) {
 
   getEvenMultiplier();
 
-  for (let i = 0; i < primeFactors.length; i++) {
-    zerosCount.push(Math.floor(count(primeFactors[i]) / primeFactors[i][1]));
+  for (let i = 0; i < basePrimeFactors.length; i++) {
+    zerosCount.push(Math.floor(count(basePrimeFactors[i]) / basePrimeFactors[i].degree));
   }
 
   return Math.min(...zerosCount);
 };
-
